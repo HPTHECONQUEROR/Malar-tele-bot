06df17ba16a2bcf4c4b06b93e632d4db9bfaee13
HARI PRASATH <104780253+HPTHECONQUEROR@users.noreply.github.com>
2024-03-02 22:35:46 +0530

    Add files via upload

---
+++ Response.py
@@ -0,0 +1,62 @@
+import requests
+import os
+
+
+class RunBot:
+    def __init__(self, user_file_path, first_name):
+        self.first_name = first_name
+        self.API_URL = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
+        self.headers = {"Authorization": "Bearer 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"}
+        self.user_file_path = user_file_path
+        self.load_prompt()
+
+    def load_prompt(self):
+        if not self.user_file_exists():
+            with open("text files/malarPrompt.txt", "r",encoding="utf-8") as file:
+                self.prompt_text = file.read()
+                file.close()
+        else:
+            with open(self.user_file_path, "r",encoding="utf-8") as user_file:
+                self.prompt_text = user_file.read()
+                user_file.close()
+
+
+    def user_file_exists(self):
+        return os.path.exists(self.user_file_path)
+
+    def print_text_after_last_occurrence(self, input_string):
+        index = input_string.rfind(self.command)
+        result = input_string[index + len(self.command):]
+        return result
+
+
+    def bot_output(self, command):
+        self.command = command
+        response = requests.post(self.API_URL, headers=self.headers,
+                                 json={"inputs": self.prompt_text + f"\n {command}"})
+        output = response.json()
+        op = output[0]["generated_text"]
+        output_final = self.print_text_after_last_occurrence(op)
+        start = output_final.find("<|assistant|>") + len("<|assistant|>")
+        end = output_final.find("<a/>")
+        output_final = output_final[start:end]
+
+        # Update prompt_text
+        self.prompt_text += f"\n <|{self.first_name}|> {command} <u/> <|assistant|> {output_final} <a/>"
+
+        with open(self.user_file_path, "w",encoding="utf-8") as user_file:
+                 user_file.write(self.prompt_text.replace("user", self.first_name) + '\n')
+                 user_file.close()
+
+
+        return output_final
+
+
+
+''''
+API_URL = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
+headers = {"Authorization": "Bearer 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"}
+
+self.API_URL = "https://api-inference.huggingface.co/models/NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO"
+        self.headers = {"Authorization": "Bearer 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"}
+'''
---
+++ main.py
@@ -0,0 +1,43 @@
+import telebot
+from Response import RunBot
+import threading
+import os
+
+BOT_TOKEN = "6748470156:AAFZ7PjTIsUE5oAKLoNfc-372QHpyHGJ6dI"
+bot = telebot.TeleBot(BOT_TOKEN)
+
+user_instances = {}
+
+
+def get_user_file(user_id, first_name):
+    file_path = f"{user_id}.txt"
+    if not os.path.exists(file_path):
+        file_path = f"{first_name}.txt"
+
+    return file_path
+
+
+def handle_user(message):
+    user_id = message.from_user.id
+    first_name = message.from_user.first_name
+
+
+    if user_id not in user_instances:
+        file_path = get_user_file(user_id, first_name)
+        user_instances[user_id] = RunBot(file_path, first_name)
+    command = message.text
+    print("given message\n"+command)
+    try:
+        reply_message = user_instances[user_id].bot_output(command)
+
+    except:
+        reply_message = "Sorry to say this, but i gotta sleep now!"
+    bot.reply_to(message, reply_message)
+    print("REPLYMESSAGE\n"+reply_message)
+
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    threading.Thread(target=handle_user, args=(message,), daemon=True).start()
+
+bot.polling()
